e457f43646f40297ca09561f0ea5cabd448cb1f0
UCSC UAV <muhammadzilahee@gmail.com>
2016-02-05 13:04:51 -0800

    Version 1

---
+++ CMakeFiles/Canny.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Canny.dir/canny.cpp.o"
+  "Canny.pdb"
+  "Canny"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Canny.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ CMakeFiles/bin/Canny.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/bin/Canny.dir/src/canny.cpp.o"
+  "bin/Canny.pdb"
+  "bin/Canny"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/bin/Canny.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
